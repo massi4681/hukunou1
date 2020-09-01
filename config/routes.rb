f2a9672703c3eb54cdc187ed2b70856429a2767b
vagrant <vagrant@localhost.localdomain>
2020-09-01 12:43:48 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
